9e21b97bd20c1a1a4746c8d04194ff59672e2ca0
Lokeshgji <lokeshkumawat435@gmail.com>
2026-03-11 12:19:23 +0530

    fix cors for vercel

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
